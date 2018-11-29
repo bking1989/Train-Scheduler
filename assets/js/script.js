5877355637769cac45b3d4b3316b7d659808320b
bking1989 <bradley_king@me.com>
2018-11-29 12:52:32 -0700

    Re-wrote JS to get local storage working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,81 @@
 $(document).ready(function() {
-    // Check to see if existing data is there
-    if (localStorage.getItem("trainCount") === null) {
-        // Counter for the number of trains
-        var trainCount = 1;
-        localStorage.setItem("trainCount", trainCount);
+    // Retrieve or create an array to store our train schedule data
+    if (localStorage.getItem("trainList") === null) {
+        var trainList = [];
+        localStorage.setItem("trainList", JSON.stringify(trainList));
     } else {
-        localStorage.getItem("trainCount");
+        var trainList = JSON.parse(localStorage.getItem("trainList"));
     };
 
-    // Check to see if schedule's been made yet
-    if (localStorage.getItem("trainSchedule") === null) {
-        return false;
-    } else {
-        localStorage.getItem("trainSchedule");
+    // Schedule render function
+    const scheduleRender = () => {
+        // Clear out the schedule's body
+        $("#scheduleArea").empty();
+
+        // Define a train counter
+        let trainCount = 1;
+
+        // Run a loop to build the new schedule
+        trainList.forEach(function(train) {
+            const newRow = $("<tr>");
+            const numberCol = $("<th class='text-center' scope='row'>")
+            const nameCol = $("<td class='text-center'>");
+            const destCol = $("<td class='text-center'>");
+            const timeCol = $("<td class='text-center'>");
+            const freqCol = $("<td class='text-center'>");
+
+            $(numberCol).append(trainCount);
+            $(nameCol).append(train.name);
+            $(destCol).append(train.destination);
+            $(timeCol).append(train.startingTime);
+            $(freqCol).append(train.frequency);
+
+            $(newRow).append(numberCol);
+            $(newRow).append(nameCol);
+            $(newRow).append(destCol);
+            $(newRow).append(timeCol);
+            $(newRow).append(freqCol);
+            $("#scheduleArea").append(newRow);
+
+            trainCount++;
+        });
     };
 
-    // Function for when form is submitted
+    // New train function
     const newTrain = () => {
-        // Retrieve the form values and define them
+        // Retrieve form data and define it
         const trainName = $("#trainName").val();
         const destination = $("#destination").val();
         const firstTime = $("#firstTime").val();
         const frequency = $("#frequency").val();
 
-        const newRow = $("<tr>");
-        const rowCount = $("<th class='text-center' scope='row'>");
-        const colOne = $("<td class='text-center'>");
-        const colTwo = $("<td class='text-center'>");
-        const colThree = $("<td class='text-center'>");
-        const colFour = $("<td class='text-center'>");
-        
-        if (trainName == "" || destination == "" || firstTime == "" || frequency == "") {
-            return false;
-        } else {
-            $(rowCount).append(trainCount);
-            $(rowCount).appendTo(newRow);
-            $(colOne).append(trainName);
-            $(colOne).appendTo(newRow);
-            $(colTwo).append(destination);
-            $(colTwo).appendTo(newRow);
-            $(colThree).append(firstTime);
-            $(colThree).appendTo(newRow);
-            $(colFour).append(frequency);
-            $(colFour).appendTo(newRow);
-
-            trainCount++;
-            localStorage.setItem("trainCount", trainCount);
-
-            $(newRow).appendTo("#scheduleArea");
-
-            const scheduleContent = $("#scheduleArea").val()
-            localStorage.setItem("trainSchedule", JSON.stringify(scheduleContent));
-
-            $("#trainName").val("");
-            $("#destination").val("");
-            $("#firstTime").val("");
-            $("#frequency").val("");
+        // Create a new object for the train's information
+        const trainObj = {
+            name : trainName,
+            destination : destination,
+            startingTime : firstTime,
+            frequency : frequency
         };
+
+        // Push this new object into an array, for storage
+        trainList.push(trainObj);
+
+        // Re-render the train schedule
+        scheduleRender();
+
+        // Clear the form's input boxes
+        $("#trainName").val("");
+        $("#destination").val("");
+        $("#firstTime").val("");
+        $("#frequency").val("");
+
+        // Finish the sequence by turning our list back into a string
+        localStorage.setItem("trainList", JSON.stringify(trainList));
     };
 
-    // Listener for the new train form's submit button
+    // Listener for the form's submit button
     $(document).on("click", "#formBtn", newTrain);
+
+    // Access local storage and render the schedule on page load
+    scheduleRender();
 });
